2aa0989babff0c54572664ade420b6251fa457b3
Stefan Bunse <sb@datenwust.net>
2012-08-19 03:47:20 +0200

    * fixed namespacing

--- src/amd.js
+++ src/amd.js
@@ -2,20 +2,25 @@
 
 (function (root, factory) {
 
+    // ensure global B4ce object
+    root.B4ce = root.B4ce || {};
+
     if (typeof exports === 'object') {
 
         var underscore = require('underscore');
 
-        module.exports = factory(underscore);
+        module.exports = factory(B4ce, underscore);
 
     } else if (typeof define === 'function' && define.amd) {
 
-        define(['underscore'], factory);
+        define(['underscore'], function (_) {
+            return factory(B4ce, _);
+        });
 
     }
-}(this, function (_) {
+}(this, function (B4ce, _) {
 
     //= b4ce.log.js
 
-    return Log;
+    return B4ce.Log;
 }));
--- src/b4ce.log.js
+++ src/b4ce.log.js
@@ -1,5 +1,7 @@
 
-Log = (function (_) {
+B4ce = B4ce || {};
+
+B4ce.Log = (function (B4ce, _) {
 //        console.log ('initializing b4ce.log...');
 
 //= b4ce.log.util.js
@@ -8,5 +10,7 @@ Log = (function (_) {
 //= b4ce.log.channel.alert.js
 //= b4ce.log.channel.console.js
 
+    B4ce.Log = Log;
+
     return Log;
-})(_);
+})(B4ce, _);

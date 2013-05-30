d4e921491a8dcc59601d74a786a2fdda9344e4fc
Stefan Bunse <sb@datenwust.net>
2013-05-30 06:00:26 +0200

    - Updated gruntfile - Extended metadata in package.json - Externalized jshint config into .jshintrc

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,72 +1,89 @@
 /*global module:false*/
 module.exports = function (grunt) {
+'use strict';
 
-    grunt.initConfig({
-        meta: {
-            version: '0.0.1',
-            banner: '// B4ce.Log, v<%= meta.version %>\n'
-                + '// Copyright (c)<%= grunt.template.today("yyyy") %> Stefan Bunse, B4ce development\n'
-                + '// Distributed under MIT license\n'
-                + '// http://github.com/b4cedev/b4ce.log\n'
+grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    meta:{
+        banner:
+            '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+            '<%= grunt.template.today("yyyy-mm-dd") %>\\n' +
+            '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+            '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %><%= pkg.author.email ? " <" + pkg.author.email + ">": "" %>;' +
+            ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
+    },
+
+    jshint: {
+        options: {
+            jshintrc: '.jshintrc',
+            force: true
         },
+        all: [
+            'Gruntfile.js',
+            'src/**/*.js',
+            '!src/amd.js',
+            '!src/b4ce.log.js'
+        ]
+    },
 
-        jshint: {
+    concat: {
+        build: {
+            src: [
+                'src/b4ce.log.util.js',
+                'src/b4ce.log.channel.js',
+                'src/b4ce.log.channel.alert.js',
+                'src/b4ce.log.channel.console.js',
+                'src/b4ce.log.js'
+            ],
+            dest: 'lib/<%= pkg.name %>.js'
+        }
+    },
+
+/*
+    rig: {
+        all: {
             options: {
-                // enforcing options
-                curly: true,
-                eqeqeq: true,
-                forin: true,
-                immed: false,
-                latedef: true,
-                newcap: true,
-                noarg: true,
-                noempty: true,
-                nonew: true,
-                quotmark: 'single',
-                undef: true,
-                unused: true,
-                trailing: true,
-                nomen: true,
-                globals: {
-                    _: false,
-                    define: false,
-                    require: false,
-                    module: false
-                }
+                banner: '<%= meta.banner %>'
             },
-            all: ['grunt.js', 'src/**/*.js']
-        },
-
-        rig: {
-            all: {
-                options: {
-                    banner: '<%= meta.banner %>'
-                },
-                files: {
-                    'lib/b4ce.log.js': ['src/b4ce.log.js'],
-                    'lib/amd/b4ce.log.js': ['src/amd.js']
-                }
-            }
-        },
-
-        uglify: {
-            'lib/b4ce.log.min.js': 'lib/b4ce.log.js',
-            'lib/amd/b4ce.log.min.js': 'lib/amd/b4ce.log.js'
-        },
-
-        watch: {
-            js: {
-                files: ['Gruntfile.js', 'src/**/*.js'],
-                tasks: ['default']
+            files: {
+                'lib/b4ce.log.js': ['src/b4ce.log.js'],
+                'lib/amd/b4ce.log.js': ['src/amd.js']
             }
         }
-    });
+    },
+*/
 
-    // Default task.
-//    grunt.registerTask('default', 'lint rig min');
-    grunt.registerTask('default', ['jshint', 'rig', 'uglify']);
+    uglify: {
+        'lib/<%= pkg.name %>.min.js': 'lib/<%= pkg.name %>.js'/*,
+        'lib/amd/b4ce.log.min.js': 'lib/amd/b4ce.log.js'*/
+    },
 
-    grunt.loadNpmTasks('grunt-contrib');
-    grunt.loadNpmTasks('grunt-rigger');
+    /**
+     * Watch task
+     */
+    watch: {
+        jshint: {
+            files: ['<%= jshint.all %>'],
+            tasks: ['jshint']
+        },
+        concat: {
+            files: ['Gruntfile.js', '<%= concat.build.src %>'],
+            tasks: ['concat']
+        },
+        uglify: {
+            files: ['Gruntfile.js', 'lib/b4ce.log.js'],
+            tasks: ['uglify']
+        }
+    }
+});
+
+// Default task.
+//    grunt.registerTask('default', ['jshint', 'rig', 'uglify']);
+grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+
+grunt.loadNpmTasks('grunt-contrib');
+//    grunt.loadNpmTasks('grunt-rigger');
+
 
 };

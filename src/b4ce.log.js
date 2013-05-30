2f1b46353d2ce3f2b4c525ebf25c8d35e41cb90e
Stefan Bunse <sb@datenwust.net>
2013-05-30 13:10:27 +0200

    - Built working UMD version of log class

--- src/b4ce.log.js
+++ src/b4ce.log.js
@@ -7,209 +7,172 @@
         define([
             'underscore',
             './b4ce.log.util',
-            './b4ce.log.channel'
+            './b4ce.log.channel',
+            './b4ce.log.channel.alert',
+            './b4ce.log.channel.console'
         ], factory);
     } else {
         // Browser globals (root is window)
-        root.B4ce.Log = factory(root._, root.B4ce.Log.Util, root.B4ce.Log.Channel);
+        root.B4ce.Log = factory(
+            root._,
+            root.B4ce.Log.Util,
+            root.B4ce.Log.Channel,
+            root.B4ce.Log.AlertChannel,
+            root.B4ce.Log.ConsoleChannel
+        );
     }
-}(this, function (_, Util, Channel) {
+}(this, function (
+    _,
+    Util,
+    Channel,
+    AlertChannel,
+    ConsoleChannel
+) {
 'use strict';
 
 // sanitize browser
 Util.sanitizeBrowser();
 
+var levels = [
+    "debug",
+    "info",
+    "notice",
+    "warning",
+    "err",
+    "crit",
+    "alert",
+    "emerg"
+];
+var levelValMax = 0;
+var levelMap = {};
+_.each(levels, function (name) {
+    levelMap[name] = ++levelValMax;
+});
+console.log('levelMap:', levelMap);
+
+var defaultChannels = {
+    alert: AlertChannel,
+    console: ConsoleChannel
+};
+
 var Log = function (options) {
-    var optKey,
-        optVal,
-        curCat,
-        curLvl,
-        curMeth,
-        channels = {},
-        channelOptions;
+    // ensure options object
+    if (_.isUndefined(options)) {
+        options = {};
+    } else if (!_.isObject(options)) {
+        throw new Error('invalid options: ' + JSON.stringify(options));
+    }
+    
+    if (options.defaultLevel) {
+        // set default category level
+        this.defaultLevel = this.sanitizeLevel(options.defaultLevel);
+    }
 
-    // prepare / normalize options
-    options = options ? _.clone(options) : {};
-
-    // define valid levels (borrowed by posix syslog standard)
-    this.levels = {
-        emerg: 8,
-        alert: 7,
-        crit: 6,
-        err: 5,
-        warning: 4,
-        notice: 3,
-        info: 2,
-        debug: 1
-    };
-
-    // define default level and category => level mapping
-    this.defaultLevel = options.defaultLevel ? this.sanitizeLevel(options.defaultLevel) : 'debug';
-    this.categoryLevel = {};
-
-
-    // create / configure categories
-
-    // default category
+    // setup categories
     this.categories = {
         common: 1
     };
-
-    // process categories option
+    this.categoryLevel = {
+        common: undefined
+    };
+    this.categoryValMax = this.categories.common;
     if (options.categories) {
-        optVal = 1;
-        for (optKey in options.categories) {
-            if (options.categories.hasOwnProperty(optKey)) {
-                if (!this.categories[optKey]) {
-                    // create custom category
-                    optVal = optVal * 2;
-                    this.categories[optKey] = optVal;
-                }
-                // set category level
-                this.categoryLevel[optKey] = options.categories[optKey] ? this.sanitizeLevel(options.categories[optKey]) : undefined;
-            }
-        }
+        this.addCategories(options.categories);
     }
-//            console.log('Log(): categories:', this.categories);
+    console.log('Log(): categories, categoryLevel:', this.categories, this.categoryLevel);
 
-    // ensure level for all categories
-    for (optKey in this.categories) {
-        if (this.categories.hasOwnProperty(optKey)) {
-            if (!this.categoryLevel[optKey]) {
-                this.categoryLevel[optKey] = this.defaultLevel;
-            }
-        }
+    // setup instance log channels
+    this.channels = {};
+    if (!_.isUndefined(options.channels)) {
+        this.addChannels(options.channels);
+    } else {
+        this.addChannels(defaultChannels);
     }
-//            console.log('Log(): categoryLevel:', this.categoryLevel);
+//    console.log('Log(): channels: ', this.channels);
 
-    // configure default channels
-    if (!options.channels) {
-        // enable default channels
-        options.channels = {};
-
-        if (Channel.Console) {
-            options.channels.console = new Channel.Console({
-                log: this
-            });
-        }
-        if (Channel.Alert) {
-            options.channels.alert = new Channel.Alert({
-                log: this
-            });
-        }
-    }
-//            console.log('Log(): channels: ', this.channels);
-
-
-    for (optKey in options.channels) {
-        if (options.channels.hasOwnProperty(optKey)) {
-            optVal = options.channels[optKey];
-            if (optVal instanceof Channel) {
-                // add prebuilt custom channel
-                channels[optKey] = optVal;
-            } else {
-                // configure default channel
-                if (!this.channels[optKey]) {
-                    throw 'Tried to configure invalid default channel: ' + optKey;
-                }
-                if (optVal !== false) {
-                    channelOptions = {
-                        log: this
-                    };
-                    if (_.isObject(optVal)) {
-                        if (optVal.level) {
-                            channelOptions.level = this.sanitizeLevel(optVal.level);
-                        }
-                        if (optVal.categories) {
-                            channelOptions.categories = this.sanitizeCategories(optVal.categories);
-                        }
-                    } else if (optVal !== true) {
-                        throw 'Invalid config for channel "' + optKey + '": ' + optVal;
-                    }
-                    channels[optKey] = new this.channels[optKey](channelOptions);
-                }
-            }
-        }
-    }
-    this.channels = channels;
-//            console.log('Log(): this.channels:', this.channels);
-
-    // create dynamic convenience methods
-    for (curCat in this.categories) {
-        if (this.categories.hasOwnProperty(curCat)) {
-            for (curLvl in this.levels) {
-                if (this.levels.hasOwnProperty(curLvl)) {
-                    if (curCat === 'common') {
-                        curMeth = curLvl;
-                    } else {
-                        curMeth = curCat + Util.capitalize(curLvl);
-                    }
-                    if (this[curMeth] !== undefined) {
-                        this.logMessage(this.categories.common, this.levels.info, 'Cannot create log method "' + curMeth + '"');
-                    } else {
-                        this[curMeth] = Util.createMethod(curCat, curLvl);
-                    }
-                }
-            }
-        }
-    }
-
+    this.createCategoryMethods();
 };
+
 _.extend(Log, {
+    /**
+     * constructor properties ("static class properties")
+     */
     Util: Util,
     Channel: Channel
 });
 
 _.extend(Log.prototype, {
     /**
-     * define "class" methods
+     * instance properties
      */
 
-    // define default channels
-    channels: {},
+    // define valid levels (borrowed by posix syslog standard)
+    levels: levelMap,
+    defaultLevel: 'debug',
+    
+    addCategory: function (name, level) {
+        if (!this.categories[name]) {
+            // create custom category
+            this.categoryValMax *= 2;
+            this.categories[name] = this.categoryValMax;
+        }
+        this.categoryLevel[name] = this.sanitizeLevel(level);
 
-    // this is the main function for logging messages. all
-    logMessage: function (categories, level, args) {
-        var msgCategories = [],
-            key,
-            i,
-            j,
-            channel,
-            channelOk,
-            channelMsgCategories;
+        return this;
+    },
 
-        categories = this.sanitizeCategories(categories);
-        level = this.sanitizeLevel(level);
+    addCategories: function (categories) {
+        var log = this;
+        if (_.isObject(categories)) {
+            _.each(categories, function (level, name) {
+                log.addCategory(name, level);
+            });
+        } else if (_.isArray(categories)) {
+            _.each(categories, function (name) {
+                log.addCategory(name);
+            });
+        } else {
+            throw new Error("categories is neither object nor array!");
+        }
 
-//                console.log('Log.logMessage(): categories, level, args:', categories, level, args);
+        return this;
+    },
 
-        // iterate over all logging categories
-        for (i = 0, j = categories.length; i < j; i = i + 1) {
-            key = categories[i];
-            // filter message categories by category level
-            if (this.levels[level] >= this.levels[this.categoryLevel[key]]) {
-                msgCategories.push(key);
+    addChannel: function (name, ChannelOptions) {
+        if (this.channels[name]) {
+            throw new Error('Channel name already registered: ' + name);
+        }
+        if (ChannelOptions instanceof Channel) {
+            // add prebuilt custom channel
+            this.channels[name] = ChannelOptions;
+        } else if (_.isFunction(ChannelOptions)) {
+            this.channels[name] = new ChannelOptions({log: this});
+        } else if (_.isFunction(defaultChannels[name])) {
+            if (ChannelOptions === false) {
+                return this;
             }
-        }
-//                console.log('Log.logMessage(): msgCategories:', msgCategories);
-        if (!msgCategories.length) {
-            // message does not pass category / level filter => return
-            return this;
+            var options = {
+                log: this
+            };
+            if (_.isObject(ChannelOptions)) {
+                _.extend(options, ChannelOptions);
+            }
+            this.channels[name] = new defaultChannels[name](options);
         }
 
-        // iterate over all channels to deliver the message
-        for (key in this.channels) {
-            if (this.channels.hasOwnProperty(key)) {
-                channel = this.channels[key];
-                channelOk = true;
-                channelMsgCategories = msgCategories;
-                // check if message passes channel filter, if so => deliver
-                if (channel.filter(msgCategories, level, args)) {
-                    channel.write(msgCategories, level, args);
-                }
-            }
+        return this;
+    },
+
+    addChannels: function (channels) {
+        var log = this;
+        if (_.isObject(channels)) {
+            _.each(channels, function (channel, name) {
+                log.addChannel(name, channel);
+            });
+        } else {
+            throw new Error("channels is neither object nor array!");
         }
-//                console.log('Log.logMessage(): categories, level, args:', categories, level, args);
+
         return this;
     },
 
@@ -243,6 +206,37 @@ _.extend(Log.prototype, {
         return this;
     },
 
+    createCategoryMethods: function (category) {
+        var categoryNames;
+        if (!_.isUndefined(category)) {
+            if (!this.categories[category]) {
+                throw new Error('invalid category: ' + JSON.stringify(category));
+            }
+            categoryNames = [category];
+        } else {
+            categoryNames = _.keys(this.categories);
+        }
+        // create dynamic convenience methods
+        var log = this;
+        _.each(categoryNames, function (curCat) {
+            _.each(levelMap, function (curVal, curLvl) {
+                var curMeth;
+                if (curCat === 'common') {
+                    curMeth = curLvl;
+                } else {
+                    curMeth = curCat + Util.capitalize(curLvl);
+                }
+                if (log[curMeth] !== undefined) {
+                    log.logMessage(log.categories.common, log.levelMap.info, 'Cannot create log method "' + curMeth + '"');
+                } else {
+                    log[curMeth] = Util.createMethod(curCat, curLvl);
+                }
+            });
+        });
+        
+        return this;
+    },
+    
     // sanitizes all acceptable forms of category specification into an array of
     // category names
     sanitizeCategories: function (categories) {
@@ -280,15 +274,18 @@ _.extend(Log.prototype, {
 
     // sanitizes all acceptable forms of level specification into a level name
     sanitizeLevel: function (level) {
-        var key;
-
+        if (_.isUndefined(level)) {
+            return level;
+        } 
         if (_.isString(level)) {
             if (!this.levels.hasOwnProperty(level)) {
-                throw 'Invalid level: ' + level;
+                throw 'Invalid string level: ' + level;
             }
             return level;
-        } else if (_.isNumber(level)) {
-            // interprete number as numeric level
+        }
+        if (_.isNumber(level)) {
+            // interprete as numeric level
+            var key;
             for (key in this.levels) {
                 if (this.levels.hasOwnProperty(key)) {
                     if (level === this.levels[key]) {
@@ -299,6 +296,51 @@ _.extend(Log.prototype, {
             throw 'Invalid numeric level: ' + level;
         }
         throw 'Invalid level: ' + level;
+    },
+
+    // this is the main function for logging messages. all
+    logMessage: function (categories, level, args) {
+        var msgCategories = [],
+            key,
+            i,
+            j,
+            channel,
+            channelOk,
+            channelMsgCategories;
+
+        categories = this.sanitizeCategories(categories);
+        level = this.sanitizeLevel(level);
+
+//                console.log('Log.logMessage(): categories, level, args:', categories, level, args);
+
+        // iterate over all logging categories
+        for (i = 0, j = categories.length; i < j; i = i + 1) {
+            key = categories[i];
+            // filter message categories by category level
+            if (this.levels[level] >= this.levels[this.categoryLevel[key] || this.defaultLevel]) {
+                msgCategories.push(key);
+            }
+        }
+//                console.log('Log.logMessage(): msgCategories:', msgCategories);
+        if (!msgCategories.length) {
+            // message does not pass category / level filter => return
+            return this;
+        }
+
+        // iterate over all channels to deliver the message
+        for (key in this.channels) {
+            if (this.channels.hasOwnProperty(key)) {
+                channel = this.channels[key];
+                channelOk = true;
+                channelMsgCategories = msgCategories;
+                // check if message passes channel filter, if so => deliver
+                if (channel.filter(msgCategories, level, args)) {
+                    channel.write(msgCategories, level, args);
+                }
+            }
+        }
+//                console.log('Log.logMessage(): categories, level, args:', categories, level, args);
+        return this;
     }
 
 });
